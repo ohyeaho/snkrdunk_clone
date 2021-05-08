c68536ff6dffad0215ad694d79f99a341ee0e6f0
ohyeaho <ohyeah7275@gmail.com>
2021-05-08 23:54:35 +0900

    market page appbar actions add

--- lib/screens/market.dart
+++ lib/screens/market.dart
@@ -5,7 +5,34 @@ class Market extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: Text('マーケット'),
+        actions: [
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  //todo
+                },
+                icon: Icon(
+                  Icons.notifications_none,
+                  color: Colors.black,
+                  size: 30,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  //todo
+                },
+                icon: Icon(
+                  Icons.article_outlined,
+                  color: Colors.black,
+                  size: 30,
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
