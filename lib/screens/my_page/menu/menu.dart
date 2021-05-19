bc365cd6895678a0354caafa3251cf13a4825fef
ohyeaho <ohyeah7275@gmail.com>
2021-05-19 17:21:47 +0900

    menu page list add

--- lib/screens/my_page/menu/menu.dart
+++ lib/screens/my_page/menu/menu.dart
@@ -3,11 +3,49 @@ import 'package:flutter/material.dart';
 class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<Widget> listTiles = <Widget>[
+      Container(
+        child: ListTile(
+          title: Text(
+            'アカウント',
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+        ),
+        decoration: BoxDecoration(
+          color: Colors.black12,
+        ),
+      ),
+      Container(
+          decoration: new BoxDecoration(
+            border: new Border(
+              bottom: new BorderSide(color: Colors.black),
+            ),
+          ),
+          child: ListTile(
+            title: Text('プロフィール'),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              Navigator.pushNamed(context, '/profile');
+            },
+          )),
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('メニュー'),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        title: Text(
+          'メニュー',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
       ),
-      body: Container(),
+      body: ListView(children: listTiles),
     );
   }
 }
