9baa7dc552abbb72dfa1309ed00771e2c1397561
ohyeaho <ohyeah7275@gmail.com>
2021-06-07 03:15:24 +0900

    search_page add

---
+++ lib/screens/search_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        backgroundColor: Colors.white,
+        title: Text(
+          '検索',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
