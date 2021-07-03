8977239ebbef737179888d365db2521684aae658
ohyeaho <ohyeah7275@gmail.com>
2021-07-04 08:49:09 +0900

    FAB 追加

--- lib/screens/media/tab/media_page_calendar.dart
+++ lib/screens/media/tab/media_page_calendar.dart
@@ -29,8 +29,59 @@ class MediaPageCalendar extends StatelessWidget {
             '7/5発売 OFF-WHITE × NIKE AIR FORCE 1 "UNIVERSITY GOLD" 抽選/定価/販売店舗まとめ',
         price: '\$150',
       ),
+      CalendarButton(
+        month: '7',
+        day: '5(月)',
+        image: 'https://cdn.snkrdunk.com/uploads/media/20210525034858-4.jpeg',
+        topicText:
+            '7/5発売 OFF-WHITE × NIKE AIR FORCE 1 "UNIVERSITY GOLD" 抽選/定価/販売店舗まとめ',
+        price: '\$150',
+      ),
+      CalendarButton(
+        month: '7',
+        day: '5(月)',
+        image: 'https://cdn.snkrdunk.com/uploads/media/20210525034858-4.jpeg',
+        topicText:
+            '7/5発売 OFF-WHITE × NIKE AIR FORCE 1 "UNIVERSITY GOLD" 抽選/定価/販売店舗まとめ',
+        price: '\$150',
+      ),
+      CalendarButton(
+        month: '7',
+        day: '5(月)',
+        image: 'https://cdn.snkrdunk.com/uploads/media/20210525034858-4.jpeg',
+        topicText:
+            '7/5発売 OFF-WHITE × NIKE AIR FORCE 1 "UNIVERSITY GOLD" 抽選/定価/販売店舗まとめ',
+        price: '\$150',
+      ),
+      CalendarButton(
+        month: '7',
+        day: '5(月)',
+        image: 'https://cdn.snkrdunk.com/uploads/media/20210525034858-4.jpeg',
+        topicText:
+            '7/5発売 OFF-WHITE × NIKE AIR FORCE 1 "UNIVERSITY GOLD" 抽選/定価/販売店舗まとめ',
+        price: '\$150',
+      )
     ];
 
-    return ListView(children: mediaPageCalendar);
+    return Scaffold(
+      body: ListView(children: mediaPageCalendar),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Container(
+        height: 40,
+        child: FloatingActionButton.extended(
+          backgroundColor: Colors.black,
+          onPressed: () {
+            //todo: ボタン処理
+          },
+          label: Text(
+            '今日に移動',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: SizedBox(),
+      ),
+    );
   }
 }
