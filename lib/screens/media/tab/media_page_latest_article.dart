56ac927595273947c7f34d768db82c54ce8a48b1
ohyeaho <ohyeah7275@gmail.com>
2021-07-04 10:26:54 +0900

    リスト項目　追加

--- lib/screens/media/tab/media_page_latest_article.dart
+++ lib/screens/media/tab/media_page_latest_article.dart
@@ -8,20 +8,54 @@ class MediaPageLatestArticle extends StatelessWidget {
   Widget build(BuildContext context) {
     List<Widget> mediaPageLatestArticle = <Widget>[
       LatestArticleButton(
-          image:
-              'https://image-cdn.hypb.st/https%3A%2F%2Fjp.hypebeast.com%2Ffiles%2F2021%2F06%2Flouis-vuitton-nike-air-force-1-release-info-4.jpg?q=80&w=1000&cbr=1&fit=max',
-          topText:
-              '【リーク】LOUIS VUITTON × NIKE AIR FORCE 1 COLLECTION 抽選/定価/販売店舗まとめ',
-          subText:
-              '誰もが知っている世界的ラグジュアリーブランドLOUIS VUITTON(ルイヴィトン)がNIKE(ナイキ)と遂にコラボレーション！2022年発売予定！2021年6月25日 追記・新たに詳細を捉えた画像が公開！昨今ラグジュ'),
+        image:
+            'https://image-cdn.hypb.st/https%3A%2F%2Fjp.hypebeast.com%2Ffiles%2F2021%2F06%2Flouis-vuitton-nike-air-force-1-release-info-4.jpg?q=80&w=1000&cbr=1&fit=max',
+        topText:
+            '【リーク】LOUIS VUITTON × NIKE AIR FORCE 1 COLLECTION 抽選/定価/販売店舗まとめ',
+        subText:
+            '誰もが知っている世界的ラグジュアリーブランドLOUIS VUITTON(ルイヴィトン)がNIKE(ナイキ)と遂にコラボレーション！2022年発売予定！2021年6月25日 追記・新たに詳細を捉えた画像が公開！昨今ラグジュ',
+      ),
       GreySpace(),
       LatestArticleButton(
-          image:
-              'https://image-cdn.hypb.st/https%3A%2F%2Fjp.hypebeast.com%2Ffiles%2F2021%2F06%2Flouis-vuitton-nike-air-force-1-release-info-4.jpg?q=80&w=1000&cbr=1&fit=max',
-          topText:
-              '【リーク】LOUIS VUITTON × NIKE AIR FORCE 1 COLLECTION 抽選/定価/販売店舗まとめ',
-          subText:
-              '誰もが知っている世界的ラグジュアリーブランドLOUIS VUITTON(ルイヴィトン)がNIKE(ナイキ)と遂にコラボレーション！2022年発売予定！2021年6月25日 追記・新たに詳細を捉えた画像が公開！昨今ラグジュ'),
+        image:
+            'https://image-cdn.hypb.st/https%3A%2F%2Fjp.hypebeast.com%2Ffiles%2F2021%2F06%2Flouis-vuitton-nike-air-force-1-release-info-4.jpg?q=80&w=1000&cbr=1&fit=max',
+        topText:
+            '【リーク】LOUIS VUITTON × NIKE AIR FORCE 1 COLLECTION 抽選/定価/販売店舗まとめ',
+        subText:
+            '誰もが知っている世界的ラグジュアリーブランドLOUIS VUITTON(ルイヴィトン)がNIKE(ナイキ)と遂にコラボレーション！2022年発売予定！2021年6月25日 追記・新たに詳細を捉えた画像が公開！昨今ラグジュ',
+      ),
+      LatestArticleButton(
+        image:
+            'https://image-cdn.hypb.st/https%3A%2F%2Fjp.hypebeast.com%2Ffiles%2F2021%2F06%2Flouis-vuitton-nike-air-force-1-release-info-4.jpg?q=80&w=1000&cbr=1&fit=max',
+        topText:
+            '【リーク】LOUIS VUITTON × NIKE AIR FORCE 1 COLLECTION 抽選/定価/販売店舗まとめ',
+        subText:
+            '誰もが知っている世界的ラグジュアリーブランドLOUIS VUITTON(ルイヴィトン)がNIKE(ナイキ)と遂にコラボレーション！2022年発売予定！2021年6月25日 追記・新たに詳細を捉えた画像が公開！昨今ラグジュ',
+      ),
+      LatestArticleButton(
+        image:
+            'https://image-cdn.hypb.st/https%3A%2F%2Fjp.hypebeast.com%2Ffiles%2F2021%2F06%2Flouis-vuitton-nike-air-force-1-release-info-4.jpg?q=80&w=1000&cbr=1&fit=max',
+        topText:
+            '【リーク】LOUIS VUITTON × NIKE AIR FORCE 1 COLLECTION 抽選/定価/販売店舗まとめ',
+        subText:
+            '誰もが知っている世界的ラグジュアリーブランドLOUIS VUITTON(ルイヴィトン)がNIKE(ナイキ)と遂にコラボレーション！2022年発売予定！2021年6月25日 追記・新たに詳細を捉えた画像が公開！昨今ラグジュ',
+      ),
+      LatestArticleButton(
+        image:
+            'https://image-cdn.hypb.st/https%3A%2F%2Fjp.hypebeast.com%2Ffiles%2F2021%2F06%2Flouis-vuitton-nike-air-force-1-release-info-4.jpg?q=80&w=1000&cbr=1&fit=max',
+        topText:
+            '【リーク】LOUIS VUITTON × NIKE AIR FORCE 1 COLLECTION 抽選/定価/販売店舗まとめ',
+        subText:
+            '誰もが知っている世界的ラグジュアリーブランドLOUIS VUITTON(ルイヴィトン)がNIKE(ナイキ)と遂にコラボレーション！2022年発売予定！2021年6月25日 追記・新たに詳細を捉えた画像が公開！昨今ラグジュ',
+      ),
+      LatestArticleButton(
+        image:
+            'https://image-cdn.hypb.st/https%3A%2F%2Fjp.hypebeast.com%2Ffiles%2F2021%2F06%2Flouis-vuitton-nike-air-force-1-release-info-4.jpg?q=80&w=1000&cbr=1&fit=max',
+        topText:
+            '【リーク】LOUIS VUITTON × NIKE AIR FORCE 1 COLLECTION 抽選/定価/販売店舗まとめ',
+        subText:
+            '誰もが知っている世界的ラグジュアリーブランドLOUIS VUITTON(ルイヴィトン)がNIKE(ナイキ)と遂にコラボレーション！2022年発売予定！2021年6月25日 追記・新たに詳細を捉えた画像が公開！昨今ラグジュ',
+      )
     ];
 
     return ListView(children: mediaPageLatestArticle);
