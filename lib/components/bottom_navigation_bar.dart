29da5dd089eef27b67f075d013c053d85eb79874
ohyeaho <ohyeah7275@gmail.com>
2021-07-05 12:58:10 +0900

    Revert "favorite ui 完成"
    This reverts commit 1763dfcbae3bbd4c4587e3cd6895b93f22e03327.

--- lib/components/bottom_navigation_bar.dart
+++ lib/components/bottom_navigation_bar.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -12,7 +11,6 @@ import 'package:snkrdunk_clone/screens/media/tab/media_page_new_post.dart';
 import 'package:snkrdunk_clone/screens/my_page/menu/menu.dart';
 import 'package:snkrdunk_clone/screens/my_page/menu/profile/profile.dart';
 import 'package:snkrdunk_clone/screens/my_page/my_page.dart';
-import 'package:snkrdunk_clone/screens/post/new_post.dart';
 import 'package:snkrdunk_clone/screens/search_page.dart';
 
 final appRoutes = {
@@ -29,199 +27,184 @@ final appRoutes = {
 };
 
 class Home extends StatelessWidget {
-  final User user = FirebaseAuth.instance.currentUser;
+  void onPressed(BuildContext context) async {
+    final Size size = MediaQuery.of(context).size;
+    double height = size.height;
+    await showModalBottomSheet(
+      backgroundColor: Colors.transparent,
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: height * 0.5,
+          decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(10.0),
+                  topLeft: Radius.circular(10.0))),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 15, bottom: 10),
+                child: Center(
+                  child: Text(
+                    'スニーカーの出品・アパレルの出品\n質問や写真を投稿することができます。',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15.0, vertical: 2),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      //todo: ボタン処理
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.ice_skating,
+                          color: Colors.black,
+                        ),
+                        Text(
+                          'スニーカーを出品する',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15.0, vertical: 2),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      //todo: ボタン処理
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                    ),
+                    child: Row(
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.tshirt,
+                          color: Colors.black,
+                        ),
+                        Text(
+                          'アパレルを出品する',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15.0, vertical: 2),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      //todo: ボタン処理
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.photo_library_outlined,
+                          color: Colors.black,
+                        ),
+                        Text(
+                          '写真や文章を投稿する',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: height * 0.08)
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    void onPressed(BuildContext context) async {
-      final Size size = MediaQuery.of(context).size;
-      double height = size.height;
-      await showModalBottomSheet(
-        useRootNavigator: true,
-        backgroundColor: Colors.transparent,
-        context: context,
-        builder: (BuildContext context) {
-          return Padding(
-            padding: const EdgeInsets.only(bottom: 50),
-            child: Container(
-              height: height * 0.5,
-              decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(10.0),
-                      topLeft: Radius.circular(10.0))),
-              child: Column(
-                // mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 15, bottom: 10),
-                    child: Center(
-                      child: Text(
-                        'スニーカーの出品・アパレルの出品\n質問や写真を投稿することができます。',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 15.0, vertical: 2),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          //todo: ボタン処理
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.white,
-                        ),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.ice_skating,
-                              color: Colors.black,
-                            ),
-                            Text(
-                              'スニーカーを出品する',
-                              style: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 15.0, vertical: 2),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          //todo: ボタン処理
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.white,
-                        ),
-                        child: Row(
-                          children: [
-                            FaIcon(
-                              FontAwesomeIcons.tshirt,
-                              color: Colors.black,
-                            ),
-                            Text(
-                              'アパレルを出品する',
-                              style: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 15.0, vertical: 2),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => NewPost(user),
-                              fullscreenDialog: true,
-                            ),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.white,
-                        ),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.photo_library_outlined,
-                              color: Colors.black,
-                            ),
-                            Text(
-                              '写真や文章を投稿する',
-                              style: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  // SizedBox(height: height * 0.08)
-                ],
-              ),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_outlined),
+            label: 'マーケット',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore_outlined),
+            label: 'メディア',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_circle_outline,
+              size: 45,
             ),
-          );
-        },
-      );
-    }
-
-    return Scaffold(
-      body: CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_bag_outlined),
-              label: 'マーケット',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.explore_outlined),
-              label: 'メディア',
-            ),
-            BottomNavigationBarItem(
-                icon: IconButton(
-                    onPressed: () => onPressed(context),
-                    icon: Icon(Icons.add_circle_outline))),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.favorite_border_rounded),
-              label: 'お気に入り',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.perm_identity),
-              label: 'マイページ',
-            ),
-          ],
-          activeColor: Colors.black,
-        ),
-        tabBuilder: (context, index) {
-          return CupertinoTabView(
-            routes: appRoutes,
-            builder: (context) {
-              switch (index) {
-                case 0:
-                  return Market();
-                  break;
-                case 1:
-                  return Media();
-                  break;
-                case 2:
-                  return CupertinoTabView(
-                    builder: (context) {
-                      return CupertinoPageScaffold(
-                          child: IconButton(
-                              onPressed: () => onPressed(context),
-                              icon: Icon(Icons.add_circle_outline)));
-                    },
-                  );
-                  break;
-                case 3:
-                  return Favorite();
-                  break;
-                case 4:
-                  return MyPage();
-                  break;
-              }
-              return Home();
-            },
-          );
-        },
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border_rounded),
+            label: 'お気に入り',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.perm_identity),
+            label: 'マイページ',
+          ),
+        ],
+        activeColor: Colors.black,
       ),
+      tabBuilder: (context, index) {
+        return CupertinoTabView(
+          routes: appRoutes,
+          builder: (context) {
+            switch (index) {
+              case 0:
+                return Market();
+                break;
+              case 1:
+                return Media();
+                break;
+              case 2:
+                return CupertinoPageScaffold(
+                    child: TextButton(
+                  child: Text('a'),
+                  onPressed: () => onPressed(context),
+                ));
+                break;
+              case 3:
+                return Favorite();
+                break;
+              case 4:
+                return MyPage();
+                break;
+            }
+            return Home();
+          },
+        );
+      },
     );
   }
 }
--- lib/screens/favorite.dart
+++ lib/screens/favorite.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 
 class Favorite extends StatelessWidget {
   final List<String> favoriteItem = ['one', 'two', 'three', 'four', 'five'];
@@ -112,37 +110,28 @@ class Favorite extends StatelessWidget {
                     decoration: BoxDecoration(
                         border: Border(top: BorderSide(color: Colors.grey))),
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
                       child: Row(
                         children: [
                           Expanded(
-                            flex: 1,
-                            child: FittedBox(
-                              child: Text(
-                                '26.5cm',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                            child: Text(
+                              '26.5cm',
+                              style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
                           Expanded(
-                            flex: 5,
                             child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
                               children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Expanded(
-                                      child: Container(
-                                        alignment: Alignment.bottomRight,
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Expanded(
                                         child: FittedBox(
                                           child: Text(
                                             '¥000,000',
-                                            // textAlign: TextAlign.end,
                                             style: TextStyle(
                                               color: Colors.red.shade600,
                                               fontWeight: FontWeight.bold,
@@ -150,52 +139,45 @@ class Favorite extends StatelessWidget {
                                           ),
                                         ),
                                       ),
-                                    ),
-                                    Expanded(
-                                      child: Container(
-                                        alignment: Alignment.topRight,
-                                        child: FittedBox(
-                                          fit: BoxFit.scaleDown,
-                                          child: Text(
-                                            '最安出品',
-                                            style: TextStyle(fontSize: 11),
-                                          ),
-                                        ),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: TextButton(
-                                    onPressed: () {
-                                      //todo: ボタン処理
-                                    },
-                                    child: Text(
-                                      '即購入',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 11,
-                                      ),
-                                    ),
-                                    style: TextButton.styleFrom(
-                                      minimumSize: Size(0, 0),
-                                      backgroundColor: Colors.grey.shade300,
-                                      primary: Colors.black,
-                                    ),
+                                      Expanded(child: Text('最安出品'))
+                                    ],
                                   ),
                                 ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Expanded(
-                                      child: Container(
-                                        alignment: Alignment.bottomRight,
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 4.0, vertical: 8.0),
+                                    child: TextButton(
+                                      onPressed: () {
+                                        //todo: ボタン処理
+                                      },
+                                      child: FittedBox(
+                                        child: Text(
+                                          '即購入',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                      style: TextButton.styleFrom(
+                                        backgroundColor: Colors.grey.shade300,
+                                        primary: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Expanded(
                                         child: FittedBox(
                                           child: Text(
                                             '¥000,000',
-                                            // textAlign: TextAlign.end,
                                             style: TextStyle(
                                               color: Colors.blue.shade600,
                                               fontWeight: FontWeight.bold,
@@ -203,41 +185,34 @@ class Favorite extends StatelessWidget {
                                           ),
                                         ),
                                       ),
-                                    ),
-                                    Expanded(
-                                      child: Container(
-                                        alignment: Alignment.topRight,
-                                        child: FittedBox(
-                                          fit: BoxFit.scaleDown,
-                                          child: Text(
-                                            '最高オファー',
-                                            style: TextStyle(fontSize: 11),
-                                          ),
-                                        ),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: TextButton(
-                                    onPressed: () {
-                                      //todo: ボタン処理
-                                    },
-                                    child: Text(
-                                      'オファー',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 11,
-                                      ),
-                                    ),
-                                    style: TextButton.styleFrom(
-                                      minimumSize: Size(0, 0),
-                                      backgroundColor: Colors.grey.shade300,
-                                      primary: Colors.black,
-                                    ),
+                                      Expanded(
+                                          child:
+                                              FittedBox(child: Text('最高オファー')))
+                                    ],
                                   ),
                                 ),
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 4.0, vertical: 8.0),
+                                    child: TextButton(
+                                      onPressed: () {
+                                        //todo: ボタン処理
+                                      },
+                                      child: FittedBox(
+                                        child: Text(
+                                          'オファー',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                      style: TextButton.styleFrom(
+                                        backgroundColor: Colors.grey.shade300,
+                                        primary: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                )
                               ],
                             ),
                           )
@@ -245,7 +220,7 @@ class Favorite extends StatelessWidget {
                       ),
                     ),
                   ),
-                )
+                ),
               ],
             ),
           );
